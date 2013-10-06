349841b054daf45c1d1ecb95483b893269cef6e8
Izhar Firdaus <izhar@inigo-tech.com>
2013-10-07 00:57:51 +0800

    update to use new twitter widget

--- wcc/twitterportlet/portlet/wcctwitterportlet.py
+++ wcc/twitterportlet/portlet/wcctwitterportlet.py
@@ -17,27 +17,13 @@ class IWCCTwitterPortlet(IPortletDataProvider):
     Define your portlet schema here
     """
     username = schema.TextLine(title=_(u'Username'), required=True)
-    height = schema.Int(title=_(u'Height'), default=300)
-    width = schema.Int(title=_(u'Width'), default=620)
-    interval = schema.Int(title=_(u'Interval'), default=7000)
-    shell_background = schema.TextLine(title=_(u'Shell background color'), default=u'#dbdbdb')
-    shell_color = schema.TextLine(title=_(u'Shell text color'), default=u"#000000")
-    tweets_background = schema.TextLine(title=_(u'Tweets background color'), default=u'#ffffff')
-    tweets_color = schema.TextLine(title=_(u'Tweets text color'), default=u'#000000')
-    tweets_link = schema.TextLine(title=_(u'Tweets link color'), default=u'#205c90')
+    widget_id = schema.TextLine(title=_(u'Widget ID'), required=True)
 
 class Assignment(base.Assignment):
     implements(IWCCTwitterPortlet)
 
     username = ''
-    height = 300
-    width = 620
-    interval = 7000
-    shell_background = '#dbdbdb'
-    shell_color = '#000000'
-    tweets_background = '#ffffff'
-    tweets_color = '#000000'
-    tweets_link = '#205c90'
+    widget_id = ''
 
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
@@ -55,47 +41,6 @@ class Renderer(base.Renderer):
     def available(self):
         return True
 
-    def script(self):
-        return """
-new TWTR.Widget({
-  version: 2,
-  type: 'profile',
-  rpp: 4,
-  interval: %(interval)s,
-  width: %(width)s,
-  height: %(height)s,
-  theme: {
-    shell: {
-      background: '%(shell_background)s',
-      color: '%(shell_color)s'
-    },
-    tweets: {
-      background: '%(tweets_background)s',
-      color: '%(tweets_color)s',
-      links: '%(tweets_link)s'
-    }
-  },
-  features: {
-    scrollbar: false,
-    loop: false,
-    live: false,
-    hashtags: true,
-    timestamp: true,
-    avatars: true,
-    behavior: 'default'
-  }
-}).render().setUser('%(username)s').start();
-        """ % {
-            'interval': self.data.interval,
-            'height': self.data.height,
-            'width': self.data.width,
-            'username': self.data.username,
-            'shell_background': self.data.shell_background,
-            'shell_color': self.data.shell_color,
-            'tweets_background': self.data.tweets_background,
-            'tweets_color': self.data.tweets_color,
-            'tweets_link': self.data.tweets_link
-        }
 
 class AddForm(base.AddForm):
     form_fields = form.Fields(IWCCTwitterPortlet)
